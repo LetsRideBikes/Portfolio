988404e9cbfb46ca69bfe1abf390ade739e4d7ca
LetsRideBikes <keithlavoieVT@gmail.com>
2022-07-16 16:05:22 -0400

    testimonials

--- src/components/AboutKeith.js
+++ src/components/AboutKeith.js
@@ -13,16 +13,15 @@ const AboutKeith = props =>{
         <h3 className="bodystyle">
 
         I'm Keith Lavoie, a New England native with roots in Burlington, Vermont. I've been working with 
-        healthcare companies for over ten years to solve unique problems and bring vision to life.  
+        healthcare companies for over ten years to solve unique problems and bring their vision to life.  
         <br/><br/> 
-        I'm currently working on software product design and management as a Solutions Architect, though as a natural jack-of-all-trades, I've worn many different hats since my career began. A persistent drive to learn new things has put me at the 
-        helm of some of the most fascinating and strategic projects and initiatives- From designing robotics workflows, to building state-of-the-art laboratory 
-        facilities, to developing innovative healthcare software. Doing so has allowed me to develop a keen sense of 
-        business interdependencies and the literacy to navigate them, as well as a method to successfully and consistently plot
-        paths to success. 
+        I'm currently working on software product design and management as a Solutions Architect, though as a natural jack-of-all-trades, I've worn many different hats since my 
+        career began. A persistent drive to learn new things has put me at the helm of some of the most fascinating and strategic projects and initiatives- From designing robotics 
+        workflows, to building state-of-the-art laboratory facilities, to developing innovative healthcare software. Doing so has allowed me to develop a keen sense of 
+        business interdependencies and the literacy to navigate them, as well as a method to efficiently and consistently plot paths to success. 
         <br/><br/> 
         When it comes to design, the principles that I tend to lean on most are those derived from the psychology of human interaction. I believe that the relationship a 
-        user has with the product they use must be authentic, empathetic and consistent. Good product design should improve a user's quality of life. 
+        user has with the product they use must be authentic, empathetic and consistent. Good product design should improve a user's quality of life.
         <br /><br />
         My personal philosophy is to always try to be surrounded by people smarter than me. The more I learn, the more I discover just how much else there is 
         to know. When I'm not immersed in a creative project with lofi beats in my headphones, you can find me behind a table saw, mountain bike 
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,11 +1,20 @@
-import bird from './img/Blackbird.png'
+
+import quotes from './img/quotes.png'
+import quotes2 from './img/quotes2.png'
+import titleimg from './img/titleimg.png'
+import man1 from './img/man1.PNG';
+import man2 from './img/man2.PNG';
+import woman1 from './img/woman1.PNG';
+import woman2 from './img/woman2.PNG';
 
 const Testimonials = props =>{
   
     return (
-<div className='testimonials-container'><h3 className="testimonial-title">TESTIMONIALS</h3>
+<div className='testimonials-container'><img className="quotes" src={quotes}/><img className="quotes2" src={quotes2}/><div className="testimonial-title"><img className="titleimage" src={titleimg} /></div>
+
+
 <div className='testimonial-entry'>
-    <img className='testimonial-photo' src={bird}></img>
+    <img className='testimonial-photo' src={man1}></img>
     <div className='testimonial-text'>
         <h3 className='titlestyle2'>JOEY SPAGHETTI</h3><h3 className="subtitlestyle modified2" style={{fontSize: "10pt"}}>CEO, Entertainment 720</h3></div>
         <div className='testimonial-body-container'><h3 className='bodystyle modified' style={{fontStyle:"italic"}}>"I loved working with Keith. He is incredibly talented and likable; not to mention charming. Women love him, men want to be him. We should all be so lucky to know such a man."</h3>
@@ -14,7 +23,7 @@ const Testimonials = props =>{
         <div className='line-spacer'></div>
 
         <div className='testimonial-entry2'>
-    <img className='testimonial-photo2' src={bird}></img>
+    <img className='testimonial-photo2' src={woman1}></img>
     <div className='testimonial-text2'>
         <h3 className='titlestyle2'>LAURA BINGO</h3><h3 className="subtitlestyle modified2" style={{fontSize: "10pt"}}>Director of Telekenisis, Heroes Inc.</h3></div>
         <div className='testimonial-body-container2'><h3 className='bodystyle modified' style={{fontStyle:"italic"}}>"When Keith worked with us, he always ate way more than his fair share of donuts. Other people would have one, maybe two, but every time I saw him he had at least six donuts. What did he even do with all of those donuts? Was he bringing them home? What the hell, Keith?"</h3>
@@ -22,7 +31,7 @@ const Testimonials = props =>{
         </div>  
         <div className='line-spacer'></div>
         <div className='testimonial-entry'>
-    <img className='testimonial-photo' src={bird}></img>
+    <img className='testimonial-photo' src={man2}></img>
     <div className='testimonial-text'>
         <h3 className='titlestyle2'>EARL RUGGS</h3><h3 className="subtitlestyle modified2" style={{fontSize: "10pt"}}>Founder, Ruggs Rugs</h3></div>
         <div className='testimonial-body-container'><h3 className='bodystyle modified' style={{fontStyle:"italic"}}>"Keith still owes me $400 dollars."</h3>
@@ -30,7 +39,7 @@ const Testimonials = props =>{
         </div> 
         <div className='line-spacer'></div>
         <div className='testimonial-entry2'>
-    <img className='testimonial-photo2' src={bird}></img>
+    <img className='testimonial-photo2' src={woman2}></img>
     <div className='testimonial-text2'>
         <h3 className='titlestyle2'>PAUL PIERCE</h3><h3 className="subtitlestyle modified2" style={{fontSize: "10pt"}}>Small Forward, Boston Celtics</h3></div>
         <div className='testimonial-body-container2'><h3 className='bodystyle modified' style={{fontStyle:"italic"}}>"Keith can really ball. I've personally seen him take down some of the best players in the game right now. I think he's up to almost 175 career triple-doubles. Russel Westbrook better watch his back!"</h3>
