98896cf54aa9b865ae1e630ae75f03c37dc3a28a
LetsRideBikes <keithlavoieVT@gmail.com>
2022-07-19 07:14:06 -0400

    animations

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@
 
 import { Routes, Route} from 'react-router-dom';
 import ClassicTheme from './ClassicTheme';
+import React, { useState, useEffect } from 'react';
 
 
 
 function App() { 
 
+    const delay = ms => new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
+
+const [toggle, setToggle] = useState(false)
+
+const handleClick = async event => { 
+  await delay(1);
+  setToggle(!toggle);
   
+}
+
+
   return (
 <div className="app-container">
 <Routes>
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -17,8 +17,8 @@ const Testimonials = props =>{
 <div className='testimonial-entry'>
     <img className='testimonial-photo' src={man1}></img>
     <div className='testimonial-text'>
-        <h3 className='titlestyle2'>JOEY SPAGHETTI</h3><h3 className="subtitlestyle modified2" style={{fontSize: "10pt"}}>CEO, Entertainment 720</h3></div>
-        <div className='testimonial-body-container'><h3 className='bodystyle modified' style={{fontStyle:"italic"}}>"I loved working with Keith. He is incredibly talented and likable; not to mention charming. Women love him, men want to be him. We should all be so lucky to know such a man."</h3>
+        <h3 className='titlestyle2'>FIRST LAST</h3><h3 className="subtitlestyle modified2" style={{fontSize: "10pt"}}>Title, Company</h3></div>
+        <div className='testimonial-body-container'><h3 className='bodystyle modified' style={{fontStyle:"italic"}}>"Placeholder testimonial"</h3>
         </div>
         </div>
         <div className='line-spacer'></div>
@@ -26,24 +26,24 @@ const Testimonials = props =>{
         <div className='testimonial-entry2'>
     <img className='testimonial-photo2' src={woman1}></img>
     <div className='testimonial-text2'>
-        <h3 className='titlestyle2'>LAURA BINGO</h3><h3 className="subtitlestyle modified2" style={{fontSize: "10pt"}}>Director of Telekenisis, Heroes Inc.</h3></div>
-        <div className='testimonial-body-container2'><h3 className='bodystyle modified' style={{fontStyle:"italic"}}>"When Keith worked with us, he always ate way more than his fair share of donuts. Other people would have one, maybe two, but every time I saw him he had at least six donuts. What did he even do with all of those donuts? Was he bringing them home? What the hell, Keith?"</h3>
+        <h3 className='titlestyle2'>FIRST LAST</h3><h3 className="subtitlestyle modified2" style={{fontSize: "10pt"}}>Title, Company</h3></div>
+        <div className='testimonial-body-container2'><h3 className='bodystyle modified' style={{fontStyle:"italic"}}>"Placeholder testimonial"</h3>
         </div>
         </div>  
         <div className='line-spacer'></div>
         <div className='testimonial-entry'>
     <img className='testimonial-photo' src={man2}></img>
     <div className='testimonial-text'>
-        <h3 className='titlestyle2'>EARL RUGGS</h3><h3 className="subtitlestyle modified2" style={{fontSize: "10pt"}}>Founder, Ruggs Rugs</h3></div>
-        <div className='testimonial-body-container'><h3 className='bodystyle modified' style={{fontStyle:"italic"}}>"Keith still owes me $400 dollars."</h3>
+        <h3 className='titlestyle2'>FIRST LAST</h3><h3 className="subtitlestyle modified2" style={{fontSize: "10pt"}}>Title, Company</h3></div>
+        <div className='testimonial-body-container'><h3 className='bodystyle modified' style={{fontStyle:"italic"}}>"Placeholder testimonial"</h3>
         </div>
         </div> 
         <div className='line-spacer'></div>
         <div className='testimonial-entry2'>
     <img className='testimonial-photo2' src={woman2}></img>
     <div className='testimonial-text2'>
-        <h3 className='titlestyle2'>PAUL PIERCE</h3><h3 className="subtitlestyle modified2" style={{fontSize: "10pt"}}>Small Forward, Boston Celtics</h3></div>
-        <div className='testimonial-body-container2'><h3 className='bodystyle modified' style={{fontStyle:"italic"}}>"Keith can really ball. I've personally seen him take down some of the best players in the game right now. I think he's up to almost 175 career triple-doubles. Russel Westbrook better watch his back!"</h3>
+        <h3 className='titlestyle2'>FIRST LAST</h3><h3 className="subtitlestyle modified2" style={{fontSize: "10pt"}}>Title, Company</h3></div>
+        <div className='testimonial-body-container2'><h3 className='bodystyle modified' style={{fontStyle:"italic"}}>"Placeholder testimonial"</h3>
         </div>
         </div>
         <button className="back-button" onClick={() => navigate(-1)}>Go back</button>
