a3af6a1a30a725ca0fa28200f58a77c4b9f5a493
LetsRideBikes <keithlavoieVT@gmail.com>
2022-07-19 13:22:22 -0400

    improvement

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ const Home = props =>{
         <div className="home_headline">
           <h1 className="text1">INTUITIVE & PURPOSEFUL<br/> DESIGN PRINCIPLES</h1>
           <h1 className="text2">COMPETENT & EXPERIENCED <br/> PROJECT MANAGEMENT</h1>
-          <h1 className="text1">DISCIPLINED AESTHETIC PASSION</h1>
+          <h1 className="text3">DISCIPLINED AESTHETIC PASSION</h1>
           </div>
           {/* <Link to="Dark"><button className="dark_mode_button">Party time!</button></Link> */}
 </div>
