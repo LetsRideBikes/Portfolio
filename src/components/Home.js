a11a3815a2740dd384b527ef0fdb1a7c22fdff34
LetsRideBikes <keithlavoieVT@gmail.com>
2022-07-28 22:49:12 -0400

    satellite

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import coverphoto from './img/bkg/fibonnaci3.svg';
 import beauty from './img/beauty2.svg';
+import lilsat from './img/lilsat.svg';
 import { Link} from 'react-router-dom';
 
 
@@ -9,13 +10,13 @@ const Home = props =>{
 <div className="home_main"    style={{ 
       backgroundImage: `url(${coverphoto})`, 
       backgroundRepeat: 'no-repeat'}}>
-
+<img className="lilsat" src={lilsat}/>
 
         <div className="home_headline">
           <h1 className="text1">Hi.</h1>
           <h1 className="text2">I'm Keith Lavoie, and I love to create</h1>
           <img className="beauty" src={beauty}/> 
-          <h1 className="text3">I have what it takes to bring your ideas to life. <br/>Check out my work and see what people say about me.</h1>
+          <h1 className="text3">I have what it takes to bring your ideas to life. <br/>Take a look at my work and read what people say about me.</h1>
           <button className="portfolio-button"><Link to="Portfolio" >CHECK OUT MY PORTFOLIO</Link></button>
           </div>
           
